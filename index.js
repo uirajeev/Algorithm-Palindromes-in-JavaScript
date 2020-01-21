eb3ecd0939919df7b7f87d7e8d876b82d2cfabf0
Rajeev Kumar Singh <rajeevkumar.web@gmail.com>
2020-01-21 02:13:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* 
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. 
+
+Algorithm Challenge
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that’s spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note. You’ll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
+
+We’ll pass strings with varying formats, such as “racecar”, “RaceCar”, and “race CAR” among others.
+*/
+
+// Solution: 1
